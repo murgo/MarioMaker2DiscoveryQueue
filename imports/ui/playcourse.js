55af7c98133ccbfa91819b08b9fb28defdb43be7
Lauri Härsilä <murgo@iki.fi>
2019-07-10 08:07:13 +0300

    Added proper Play Course logic

--- imports/ui/playcourse.js
+++ imports/ui/playcourse.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { IsLoggedIn } from "./profile.js";
 
 import './playcourse.html';
+import { Tickets } from '../api/tickets.js';
 
 Template.playCourse.helpers({
     hasReservedCourse() {
@@ -15,7 +16,24 @@ Template.playCourse.helpers({
         }
 
         let reservedTicketId = userData.reservedTicketId;
-        return reservedTicketId != null;
+        return reservedTicketId != null && reservedTicketId != "";
+    },
+    getReservedCourse() {
+        if (!IsLoggedIn()) {
+            return null;
+        }
+
+        var userData = Meteor.user();
+        if (userData == null) {
+            return null;
+        }
+
+        let reservedTicketId = userData.reservedTicketId;
+        if (reservedTicketId == null) {
+            return null;
+        }
+
+        return Tickets.findOne({_id: reservedTicketId});
     },
 });
 
@@ -35,11 +53,40 @@ Template.playCourse.events({
                 sAlert.error(err.error);
             } else {
                 if (result) {
-                    sAlert.success("Course reserved! Go play it!");
+                    sAlert.success("Course reserved! Go play!");
                 } else {
-                    sAlert.info("No suitable courses in queue, submit some y'all.");
+                    sAlert.info("No suitable courses in queue. Go ahead and submit some courses for others!");
                 }
             }
         });
     },
+
+    'submit .course-played'(event, instance) {
+        // Prevent default browser form submit
+        event.preventDefault();
+     
+        if (!IsLoggedIn(true)) {
+            return;
+        }
+
+        // Get value from form element
+        const target = event.target;
+        const courseStyle = target.courseStyle.value;
+
+        // Reserve course
+        Meteor.call("tickets.coursePlayed", courseStyle, (err, result) => {
+            if (err) {
+                console.log(err);
+                sAlert.error(err.error);
+            } else {
+                if (result > 0) {
+                    sAlert.success("You earned a coin! Ka-ching!");
+                } else if (result == 0) {
+                    sAlert.info("Too bad :|");
+                } else {
+                    sAlert.error("Incorrect course style! You lose a coin.")
+                }
+            }
+        });
+    },    
 });
